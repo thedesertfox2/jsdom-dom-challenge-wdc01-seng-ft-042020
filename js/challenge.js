e945a9e805659e65228a8d890c863b5db2d785e5
Injae Lee <github email address>
2020-06-05 18:45:13 -0400

    working on pause

--- js/challenge.js
+++ js/challenge.js
@@ -1,2 +1,46 @@
+document.addEventListener("DOMContentLoaded", openFunction)
+
+
+function myCallBack()
+{   
+    let counter = document.querySelector("#counter") 
+    let number = counter.innerHTML 
+        number++ 
+    counter.innerHTML = number    
+}
+
+
+function openFunction(){
+    let intervalID = window.setInterval(myCallBack, 1000);
+
+    let minus = document.querySelector("#minus")
+    minus.addEventListener("click", handleMinus)
+
+    let plus = document.querySelector("#plus")
+    plus.addEventListener("click", myCallBack)
+
+    let pause = document.querySelector("#pause")
+    pause.addEventListener("click", handlePause)
+}
+
+function handleMinus(e){
+    let counter = document.querySelector("#counter") 
+    let number = counter.innerHTML 
+        number--
+    counter.innerHTML = number  
+}
+
+function handlePause(e){
+    let pause = document.querySelector("#pause")
+    pause.innerHTML = 
+
+}
+
+
+
+
+
+
+
 
 

8a3556018492d03bd1cf683a8e33e0b35bf2a8f8
thedesertfox2 <alexander.gautherin@uconn.edu>
2020-06-06 13:42:06 -0400

    working pause button

--- js/challenge.js
+++ js/challenge.js
@@ -1,18 +1,23 @@
-document.addEventListener("DOMContentLoaded", openFunction)
+// const intervalID = window.setInterval(myCallBack, 1000);
+
+document.addEventListener("DOMContentLoaded", () => {
+    openFunction()
+})
 
 
-function myCallBack()
-{   
+function myCallBack() {   
     let counter = document.querySelector("#counter") 
-    let number = counter.innerHTML 
+    let pause = document.querySelector('#pause')
+    if (pause.innerText === 'Pause') {
+        let number = counter.innerHTML 
         number++ 
-    counter.innerHTML = number    
+        counter.innerHTML = number 
+    }   
 }
 
 
 function openFunction(){
-    let intervalID = window.setInterval(myCallBack, 1000);
-
+    const intervalID = window.setInterval(myCallBack, 1000);
     let minus = document.querySelector("#minus")
     minus.addEventListener("click", handleMinus)
 
@@ -20,21 +25,51 @@ function openFunction(){
     plus.addEventListener("click", myCallBack)
 
     let pause = document.querySelector("#pause")
-    pause.addEventListener("click", handlePause)
+    pause.innerText = 'Pause'
+    pause.addEventListener("click", function () {handlePause(intervalID)})
 }
 
 function handleMinus(e){
-    let counter = document.querySelector("#counter") 
-    let number = counter.innerHTML 
+    let counter = document.querySelector("#counter")
+    let pause = document.querySelector('#pause')
+    if (pause.innerText === 'Pause') {
+        let number = counter.innerHTML 
         number--
-    counter.innerHTML = number  
+        counter.innerHTML = number 
+    }    
+     
 }
 
-function handlePause(e){
+function handlePause (id){
+    
+    let pausing = clearInterval(id)
+    
+
     let pause = document.querySelector("#pause")
-    pause.innerHTML = 
+    pause.parentNode.removeChild(pause)
+
+    let stopp = document.createElement('button')
+    stopp.setAttribute('id', 'pause')
+
+    stopp.innerText = 'Resume'
+    document.body.appendChild(stopp)
+
+    stopp.addEventListener("click", openFunction)
+    
 
 }
+
+// function handleResume (e) {
+//     debugger
+    
+//     let pause = document.querySelector("#pause")
+//     pause.innerText = 'Pause'
+//     return openFunction()
+
+    
+    
+
+// }
 
 
 
